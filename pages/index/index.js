7eb44fcfc2e454bae2c442ae84f7e4eca4f8aad3
houyuli <houyuli@jd.com>
2018-09-08 13:53:19 +0800

    '首页更新'

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,55 +3,50 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        searchKey: '连衣裙',
+        pageData: []
+    },
+    onLoad: function () {
+        this.fnGetPageData()
+    },
+    fnSearch(data) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            searchKey: data.detail.keyword
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+      // console.log('查看data', data.detail.keyword)
+        this.fnGetPageData()
+    },
+    fnGetPageData () {
+        // console.log('查看data', this.searchKey, this)
+        wx.request({
+            url:"https://hackathon.jd.com/hackaton//product/query",
+            method: 'POST',
+            data: {
+                userPin: 'test1',
+                searchKeywords: this.data.searchKey
+            },
+            success: res => {
+                console.log('获取接口数据成功', res)
+                const data = res.data
+                if (data && data.success) {
+                    this.setData({
+                        pageData: data.data
+                    })
+                }
+            },
+            fail: err => {
+
+            }
+        })
+    },
+    fnGoDeatil() {
+        wx.navigateTo({
+            url: '/pages/product-detail/product-detail'
+        })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  fnSearch (data) {
-    console.log('开始搜索', data)
-  }
 })
